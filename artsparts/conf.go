73966544210cb2576f5bf9dad7d1bd595376923b
Andreas Schröpfer <andreas@apxp.info>
2017-06-18 10:47:08 +0200

    Fix test fails

--- artsparts/conf.go
+++ artsparts/conf.go
@@ -2,20 +2,9 @@ package main
 
 import (
 	"io/ioutil"
-	"log"
 
 	"gopkg.in/yaml.v2"
 )
-
-var conf Conf
-
-func init() {
-	var err error
-	conf, err = loadConf(confFile)
-	if err != nil {
-		log.Fatal("Error loading conf", err)
-	}
-}
 
 type Conf struct {
 	ServerPort    string `yaml:"server_port,omitempty"`

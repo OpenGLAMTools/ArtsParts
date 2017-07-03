29e0d6555dc5e672db3320b1abcfea46f4d9dd48
Andreas Schröpfer <andreas@apxp.info>
2017-07-03 23:18:34 +0200

    Layout editor

--- artsparts/templates/lib/editor.js
+++ artsparts/templates/lib/editor.js
@@ -26,7 +26,7 @@ var app = new Vue({
    delimiters: ['[[', ']]'],
     el: '#editor',
     data: {
-        tweettext: "abc"
+        tweettext: "@OpenArtsParts"
     },
     computed: {
         charsRemain: function () {
